3eee3f46e37014ee06af61b19eb43e7701e5b2a8
Daniel Agar <daniel@agar.ca>
2020-07-20 11:46:23 -0400

    vtol_att_control: limit excessive scheduling

--- src/modules/vtol_att_control/vtol_att_control_main.cpp
+++ src/modules/vtol_att_control/vtol_att_control_main.cpp
@@ -51,6 +51,7 @@
 #include <uORB/Publication.hpp>
 
 using namespace matrix;
+using namespace time_literals;
 
 VtolAttitudeControl::VtolAttitudeControl() :
 	WorkItem(MODULE_NAME, px4::wq_configurations::rate_ctrl),
@@ -341,6 +342,15 @@ VtolAttitudeControl::Run()
 		exit_and_cleanup();
 		return;
 	}
+
+	const hrt_abstime now = hrt_absolute_time();
+
+	// prevent excessive scheduling (> 500 Hz)
+	if (now - _last_run_timestamp < 2_ms) {
+		return;
+	}
+
+	_last_run_timestamp = now;
 
 	if (!_initialized) {
 		parameters_update();  // initialize parameter cache
